0c1d893517749cd2003ac035db559077b3f16455
Shailedra-RapidQuest <shailendratrivedi009@gmail.com>
2024-04-15 21:25:30 +0530

    Template is ready by removing all the boilerplates

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import NavbarComponent from "./components/Navbar.vue"
+import FirstSection from './components/FirstSection.vue'
+</script>
+
+<template>
+  <NavbarComponent/>
+  <FirstSection />
+</template>
+

b9d39e35e8267cc65aa6d57b1bd476c276fe207b
Shailedra-RapidQuest <shailendratrivedi009@gmail.com>
2024-04-15 21:26:45 +0530

    Tempalte is ready by removing all the boilerplates

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-import NavbarComponent from "./components/Navbar.vue"
-import FirstSection from './components/FirstSection.vue'
+
 </script>
 
 <template>
-  <NavbarComponent/>
-  <FirstSection />
+  <div>Shailendra</div>
 </template>
 

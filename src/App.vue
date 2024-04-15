ebdcf00ea4a90c19e7e861b9b6a1dcbaf854e4b9
Shailedra-RapidQuest <shailendratrivedi009@gmail.com>
2024-04-15 21:38:33 +0530

    Adding the second section of the landing page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar_Component from './components/Navbar.vue'
 import FirstSection from './components/FirstSection.vue'
+import SecondSection from "./components/SecondSection.vue"
 </script>
 
 <template>
   <Navbar_Component />
   <FirstSection />
+  <SecondSection />
 </template>
---
+++ src/components/SecondSection.vue
@@ -0,0 +1,32 @@
+<script>
+import '../assets/css/second_section.css'
+import HotJar from "../assets/icons/HotJar.vue"
+export default {
+    name: 'SecondSection',
+    components: {
+        HotJar: HotJar
+    }
+}
+</script>
+<template>
+    <section class="second_section">
+        <div>USED BY OVER 50 ENTERPRISES</div>
+        <ul>
+            <li>
+                <HotJar />
+            </li>
+            <li>
+                <HotJar />
+            </li>
+            <li>
+                <HotJar />
+            </li>
+            <li>
+                <HotJar />
+            </li>
+            <li>
+                <HotJar />
+            </li>
+        </ul>
+    </section>
+</template>

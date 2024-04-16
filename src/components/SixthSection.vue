91dcbef76a081f353fa00c904d0daddca2951245
Shailedra-RapidQuest <shailendratrivedi009@gmail.com>
2024-04-16 12:49:24 +0530

    Fixing some small bugs

--- src/components/SixthSection.vue
+++ src/components/SixthSection.vue
@@ -91,4 +91,102 @@ export default {
     order: 0;
     flex-grow: 0;
 }
+
+.input_values {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
+    gap: 16px;
+    width: 689px;
+    height: 80px;
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+}
+
+.input_values div:nth-child(1) {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0px;
+    gap: 16px;
+    width: 689px;
+    height: 44px;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 0 auto;
+}
+
+.input_values div:nth-child(2) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0px;
+    gap: 12px;
+    width: 265px;
+    height: 20px;
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+    margin: 0 auto;
+}
+
+.input_values div:nth-child(2)>p {
+    width: 130px;
+    height: 20px;
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 128%;
+    color: #64748b;
+    flex: none;
+    flex-grow: 0;
+}
+
+.input_values div:nth-child(2)>span {
+    width: 6px;
+    height: 6px;
+    background: #94a3b8;
+    border-radius: 100%;
+    flex: none;
+    flex-grow: 0;
+}
+
+.input_values div:nth-child(1)>input:focus {
+    outline: none;
+}
+
+.input_values div:nth-child(1)>input {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 12px 16px;
+    gap: 10px;
+    background: #f1f5f9;
+    border: 1px solid #cbd5e1;
+    border-radius: 4px;
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 128%;
+    color: #94a3b8;
+}
+
+.input_values div:nth-child(1) input:nth-child(1) {
+    width: 200px;
+    height: 44px;
+}
+
+.input_values div:nth-child(1) input:nth-child(2) {
+    width: 352px;
+    height: 44px;
+}
 </style>
